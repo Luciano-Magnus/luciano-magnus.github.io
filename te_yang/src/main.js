ef652d7ac1a89168da03b2e65dd8b417bf91d695
luciano-cervantes <44214529+Luciano-Magnus@users.noreply.github.com>
2023-06-20 14:29:42 -0300

    Remove te_yang

--- te_yang/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-
-createApp(App).mount('#app')
